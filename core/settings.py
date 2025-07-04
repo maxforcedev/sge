d96c06b9a2ea1a1c23d9f26906690f21e4f34bd3
Luis Felipe <lfcvs.rj@gmail.com>
2025-07-04 11:43:40 -0300

    create apps models

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'brands',
+    'categories',
+    'suppliers',
+    'products',
+    'inflows',
+    'outflows'
 ]
 
 MIDDLEWARE = [
